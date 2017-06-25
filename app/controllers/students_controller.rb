c608e7a7479529b2c4ba79e61a851585d7b81d98
Chrissy Ha <christinayha@gmail.com>
2017-06-25 20:36:44 +0000

    Edit Chrissy's bio

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,7 @@
 class StudentsController < ApplicationController
 def chrissy
     @bio = "Grew up in NJ & HK."
-    @restaurant= "Small Cheval"
+    @food= "Small Cheval"
     @tv="(movies)"
 end
 end
