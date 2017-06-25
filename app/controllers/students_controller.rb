69502001b2dbcbe3aecf8c8c8b66c97541f0920a
Dahlia Santamaria <dasantamaria@cps.edu>
2017-06-25 20:12:11 +0000

    that info

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,8 @@
 class StudentsController < ApplicationController
-
+    def dahlia
+    @bio="I love chicken and videogames.I got decent style."
+    @restaurant="Kfc"
+    @tv="Supergirl"
+    @name="Dahlia"
+    end
 end
