230c5427b42aef876aa9ff6ceafc12b6e590f43b
Naomi <naomi.mine@gmail.com>
2017-06-25 20:24:24 +0000

    first commit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,8 @@
 class StudentsController < ApplicationController
-
+    def naomi 
+        @full_name = "Naomi Mine"
+        @bio = "Naomi was born in 1992 to two Hyde Park natives, Rachel and Andrew. The rest is history."
+        @restaurant = "Tanoshii"
+        @tv = "Kimmy Schmidt"
+    end
 end
